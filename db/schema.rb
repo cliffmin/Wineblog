200c6f34127e05abafbbb91f77dcd2651b3993e5
Cliff <cliffmin@gmail.com>
2013-12-19 11:16:48 -0700

    working app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131217032647) do
+ActiveRecord::Schema.define(version: 20131219025013) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -43,6 +43,22 @@ ActiveRecord::Schema.define(version: 20131217032647) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+  end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
