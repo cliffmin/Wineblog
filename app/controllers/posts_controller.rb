593b7080d063fde1da90fb7c77f6c1ce3f1a8ac0
Cliff <cliffmin@gmail.com>
2014-01-02 20:34:28 -0700

    put in bootstrap signin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def index
     if params[:search]
       @posts = Post.search(params[:search]).order("created_at DESC")
-    el  sif params[:tag]
+    elsif params[:tag]
       @posts = Post.tagged_with(params[:tag])
     else
       @posts = Post.all.order('created_at DESC')
